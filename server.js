12329e5bcedd9332329dc5193456a8db01d7e38f
Ankit Kumar <ankit@ipapagari.com>
2017-02-22 02:36:13 +0530

    port and https update

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 5100;
 
-const PORT = 5100;
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect(`http://${req.hostname}${req.url}`);
+  }
+  next();
+});
 
 app.use(express.static('public'));
 
-app.listen(PORT, () => {
-    console.info(`your app is running at http://localhost:${PORT}`);
+app.listen(PORT, function () {
+  console.log('Your app is up and running at http://localhost:'+PORT);
 });
